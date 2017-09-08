89c4e89a76a5176d789c05877884629fc4cc3a38
leihaha <leiqiaojuan81@gmail.com>
2017-09-08 14:43:27 +0800

    Chap_2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -15,6 +16,7 @@ class Post(models.Model):
     
     objects = models.Manager()       # The default manager
     published = PublishedManager()     # Our custom manager
+    tags = TaggableManager()
 
     STATUS_CHOICES = (
             ('draft', 'Draft'),
@@ -53,3 +55,4 @@ class Comment(models.Model):
     
     def __str__(self):
         return 'Comment by {} on {}'.format(self.name, self.post)
+
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
 from .models import Post, Comment
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+
     paginator = Paginator(object_list, 3)   # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -19,9 +28,9 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
+    
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
-
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
@@ -43,7 +52,12 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    # List of similar postd
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
